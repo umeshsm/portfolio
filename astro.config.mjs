93222132aba72462ef0bf2589c8af910cb839f80
Umesh S M <umeshsmg007@gmail.com>
2025-01-06 23:37:49 +0530

    feat: add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
+import partytown from '@astrojs/partytown'
 import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
 import solidJs from '@astrojs/solid-js'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://umeshsm.com',
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })]
+  integrations: [
+    mdx(),
+    sitemap(),
+    solidJs(),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    partytown({
+      config: { forward: ['dataLayer.push'] }
+    })
+  ]
 })
